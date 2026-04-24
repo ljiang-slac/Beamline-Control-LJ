6576aef39d88d75a4d9cc1913b29643e1a5f76e7
ljiang-slac <ljiang@slac.stanford.edu>
2026-04-24 08:47:38 -0700

    updated json file

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+export http_proxy="http://134.79.44.164:3128"
+export https_proxy="http://134.79.44.164:3128"
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+export NO_PROXY=localhost,127.0.0.1
+
+
+source .venv311/bin/activate
+
